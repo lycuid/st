833d94410d138b19d32c69e489222087e81466bd
Abhishek Kadam <abhishekk19@gmail.com>
2021-03-09 05:01:35 +0530

    M-[k/j/u/d] for scrolling, retabbed, refactor.

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "BlexMono Nerd Font:size=11:antialias=true:autohint=true";
+static char *font = "FiraCode Nerd Font:size=11:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -220,7 +220,6 @@ static MouseShortcut mshortcuts[] = {
 };
 
 /* Internal keyboard shortcuts. */
-#define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
 
 static Shortcut shortcuts[] = {
@@ -237,8 +236,10 @@ static Shortcut shortcuts[] = {
   { TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
   { ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
   { TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-  { ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-  { ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+  { Mod1Mask,             XK_u,           kscrollup,      {.i = -1} },
+  { Mod1Mask,             XK_d,           kscrolldown,    {.i = -1} },
+  { Mod1Mask,             XK_k,           kscrollup,      {.i =  1} },
+  { Mod1Mask,             XK_j,           kscrolldown,    {.i =  1} },
 };
 
 /*
